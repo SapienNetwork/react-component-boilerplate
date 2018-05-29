ebe8a9db3c1ed01011e6fe83ab51fcd6a9a1af28
Ethan Escareno Rosano <ethan.rosanoo@gmail.com>
2018-05-28 22:19:21 -0500

    Beter install-peers script

--- build/install-peers.js
+++ build/install-peers.js
@@ -1,3 +1,4 @@
+const ora = require('ora');
 const path = require('path');
 const pkt = require('../package.json');
 const cp = require('child_process');
@@ -7,16 +8,18 @@ const { peerDependencies } = pkt;
 
 const rootFolder = path.join(__dirname, '..');
 
+const spinner = ora('Installing Peers as Dev...');
+spinner.start();
 
 const peersNames = Object.entries(peerDependencies)
   .map(d => d.join('@'))
   .join(' ');
 
   executioner(`yarn add --peer --no-lockfile ${peersNames}`, {}, (error, result) => {
+    spinner.stop();
     if (error) {
       console.error('Unable to install peerDependencies', error);
       process.exit(1);
       return;
     }
-    console.log('Done!');
   });
