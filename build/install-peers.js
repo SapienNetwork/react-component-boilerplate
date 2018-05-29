f86bf62d9e6a5eadd659ee0cc533c802abf9d12a
Ethan Escareno Rosano <ethan.rosanoo@gmail.com>
2018-05-28 22:11:28 -0500

    Adding better logic for install peers

--- build/install-peers.js
+++ build/install-peers.js
@@ -1,22 +1,22 @@
 const path = require('path');
 const pkt = require('../package.json');
 const cp = require('child_process');
+var executioner = require('executioner');
 
 const { peerDependencies } = pkt;
+
+const rootFolder = path.join(__dirname, '..');
+
 
 const peersNames = Object.entries(peerDependencies)
   .map(d => d.join('@'))
   .join(' ');
 
-const rootFolder = path.join(__dirname, '..');
-
-cp.spawn('yarn', [
-  'add',
-  '-P',
-  '--no-lockfile',
-  peersNames
-], {
-  cwd: rootFolder,
-  stdio: 'inherit',
-  env: process.env,
-});
+  executioner(`yarn add --peer --no-lockfile ${peersNames}`, {}, (error, result) => {
+    if (error) {
+      console.error('Unable to install peerDependencies', error);
+      process.exit(1);
+      return;
+    }
+    console.log('Done!');
+  });
